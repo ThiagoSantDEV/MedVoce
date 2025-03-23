c2ab242a1b0461e29f1704aa5342709df9f594c1
AlexandreFideles-S <alexandre.fideles@hotmail.com>
2025-03-23 18:29:26 -0300

    Formulario para user

---
+++ medvc/src/main/java/clinic/medvc/controller/TestController.java
@@ -0,0 +1,14 @@
+package clinic.medvc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    @GetMapping("/endpoint")
+    public String getData() {
+        return "Hello from Spring Boot!";
+    }
+}
--- medvc/src/main/java/clinic/medvc/controller/User/UserController.java
+++ medvc/src/main/java/clinic/medvc/controller/User/UserController.java
@@ -7,28 +7,20 @@ import clinic.medvc.service.UserService;
 
 import java.net.URI;
 
+
 @RestController
-@RequestMapping("v1/users")
-public class UserController
-{
+@RequestMapping("/v1/users")
+public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
-    public  UserController(UserService userService){
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody CreateUserDto createUserDto){
-
-
+    @PostMapping("/userRegistration")
+    public ResponseEntity<User> createUser(@RequestBody CreateUserDto createUserDto) {
         var userId = userService.createUser(createUserDto);
         return ResponseEntity.created(URI.create("/v1/users/" + userId.toString())).build();
     }
-
-    @GetMapping("/{userId}")
-    public ResponseEntity<User> getUserById(@PathVariable("userId") String userId){
-       return null;
-    }
-
 }
